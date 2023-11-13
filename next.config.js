f200f2e1c1864db7dbd8a18fab7df4683fbf57ec
L&H <abc@hub.moe>
2023-11-13 19:13:28 +0800

    feat: build csr only

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    assetPrefix: isProd ? '/react-project/out/' : undefined,
+}
 
 module.exports = nextConfig
